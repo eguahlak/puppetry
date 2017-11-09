754cd03038f730458fea2432884190393e2ea488
Christian Gram Kalhauge <christian@kalhauge.dk>
2017-11-09 15:41:57 +0100

    More streamlined events

--- puppet-client/src/main/elm/Main.elm
+++ puppet-client/src/main/elm/Main.elm
@@ -1,14 +1,18 @@
 --import Color exposing (Color, rgb, toRgb, toHsl)
-import Html exposing (Html)
+import Html exposing (..)
 -- import Json.Decode as Decode
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 --import Svg.Events exposing (..)
---import Mouse exposing (position, Position, moves)
+import Mouse exposing (position, Position, moves)
 -- import Html exposing (Html)
 import Puppetry.ColorSelect as ColorSelect
+import List
 
-type alias Model = ColorSelect.Model
+-- import Touch
+-- import SingleTouch
+
+type alias Model = List (ColorSelect.Model)
 type alias Msg = ColorSelect.Msg
 
 
@@ -22,18 +26,34 @@ main =
     }
 
 init : ( Model, Cmd Msg )
-init = ColorSelect.init
+init =
+    ([ ColorSelect.default "front" 100 100
+     , ColorSelect.default "back" 300 300
+     ]
+    , Cmd.none
+    )
 
 update
   : Msg
   -> Model
   -> ( Model, Cmd Msg )
-update = ColorSelect.update
+update msg ms =
+    (List.map (ColorSelect.update_ msg ) ms, Cmd.none)
 
 subscriptions : Model -> Sub Msg
-subscriptions = ColorSelect.subscriptions
+subscriptions model =
+    Sub.batch
+        [ Mouse.moves
+            (\pos -> ColorSelect.Move ({x = toFloat pos.x, y = toFloat pos.y}))
+        , Mouse.ups
+            (\_ -> ColorSelect.SelectionDone )
+        ]
 
 testView : Model -> Html Msg
-testView m =
-  svg [ width "400", height "400"]
-    [ ColorSelect.view m ColorSelect.SetColor ]
+testView ms =
+    div
+    []
+    [ svg
+      [ width "400", height "400"]
+      (List.map (ColorSelect.view) ms)
+    ]
--- puppet-client/src/main/elm/Puppetry/ColorSelect.elm
+++ puppet-client/src/main/elm/Puppetry/ColorSelect.elm
@@ -1,52 +1,85 @@
 module Puppetry.ColorSelect exposing (..)
 
 import Color exposing (Color, rgb, toRgb, toHsl)
-import Html exposing (Html)
+-- import Html exposing (Html)
 -- import Json.Decode as Decode
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
-import Mouse exposing (position, Position, moves)
+-- import Mouse exposing (position, Position, moves)
 
 type alias Selection =
-  { dist : Float, angle : Float}
+  { dist : Float
+  , angle : Float
+  }
 
 type alias Model =
-  { color : Color
+  { name : String
+  , color : Color
   , selection : Maybe Selection
+  , isOn : Bool
   , position : Position
   }
 
-init : (Model, Cmd Msg)
-init =
-    ( { position = { x = 200, y = 200 }
-      , color = rgb 128 128 0
-      , selection = Nothing
-      }
-    , Cmd.none )
+type alias Position =
+    { x : Float
+    , y : Float
+    }
+
+
+default : String -> Float -> Float -> Model
+default name x y =
+  { name = name
+  , position = {x = x, y = y}
+  , color = rgb 128 128 0
+  , isOn = True
+  , selection = Nothing
+  }
 
 type Msg
-  = SetColor Color
-  | SetSelectionMode (Maybe Selection)
+  = Move Position
+  | SelectionDone
+  | SelectionStart String
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  case model.selection of
-    Nothing -> Sub.none
-    Just x ->
-      Sub.batch
-        [ Mouse.moves
-              (\pos ->
-                   let vect = { x = toFloat (pos.y - model.position.y)
-                              , y = toFloat (pos.x - model.position.x)
-                              }
-                       angle = atan2 vect.x vect.y
-                   in SetSelectionMode (Just { angle = atan2 vect.x vect.y
-                                             , dist = sqrt (vect.x^2 + vect.y^2)
-                                             })
-              )
-        , Mouse.ups (\_ -> if x.dist < 20 then SetSelectionMode Nothing else SetColor (colorFromSelection x))
-        ]
+
+update_ : Msg -> Model -> Model
+update_ msg model =
+    case model.selection of
+        Just sel ->
+            case msg of
+                Move pos ->
+                    { model | selection = Just (selectionFromPosition pos model)}
+                SelectionDone ->
+                    if sel.dist < 20
+                    then { model | isOn = not model.isOn, selection = Nothing}
+                    else if sel.dist > 120
+                    then { model | selection = Nothing}
+                    else { model | color = colorFromSelection sel, selection = Nothing}
+                SelectionStart name ->
+                    model
+        Nothing ->
+            case msg of
+                 SelectionStart name ->
+                     if model.name == name
+                     then { model | selection = Just ({dist = 0, angle = 0})}
+                     else model
+                 _ ->
+                     model
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = (update_ msg model, Cmd.none)
+
+selectionFromPosition : Position -> Model -> Selection
+selectionFromPosition pos model =
+    let vect =
+            { x = pos.y - model.position.y
+            , y = pos.x - model.position.x
+            }
+    in
+        { angle = atan2 vect.x vect.y
+        , dist = sqrt (vect.x^2 + vect.y^2)
+        }
 
 
 colorFromSelection : Selection -> Color
@@ -60,17 +93,9 @@ selectionFromColor color =
         dist = (hsl.lightness) * 100 + 20
     in Selection dist (hsl.hue)
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update color model =
-  case color of
-    SetColor color ->
-      ( { model | color = color, selection = Nothing}, Cmd.none)
-    SetSelectionMode b ->
-      ( { model | selection = b }, Cmd.none)
 
-
-view : Model -> (Color -> Msg) -> Svg Msg
-view current f =
+view : Model -> Svg Msg
+view current =
   let color = toRgb current.color
   in g [ transform ("translate("
              ++ toString current.position.x ++ ","
@@ -78,42 +103,42 @@ view current f =
       ((case current.selection of
            Just sel ->
             let csel = selectionFromColor current.color
-            in [
-                circle
-                    [ stroke (colorFromSelection sel |> toRgb |> colorToCss)
-                    , strokeWidth "5px"
-                    , fill "none"
-                    , r (toString sel.dist)
-                    ] []
-                , circle
-                    [ stroke (current.color |> toRgb |> colorToCss)
-                    , strokeWidth "2px"
-                    , fill "none"
-                    , r (toString csel.dist)
-                    ] []
-                , line
-                    [ stroke (colorFromSelection sel |> toRgb |> colorToCss)
-                    , strokeWidth "5px"
-                    , x2 (toString ((cos sel.angle) * sel.dist) ++ "px")
-                    , y2 (toString ((sin sel.angle) * sel.dist) ++ "px")
-                    ] []
-                , line
-                    [ stroke (current.color |> toRgb |> colorToCss)
-                    , strokeWidth "2px"
-                    , x2 (toString ((cos csel.angle) * csel.dist) ++ "px")
-                    , y2 (toString ((sin csel.angle) * csel.dist) ++ "px")
-                    ] []
-           ]
+            in (drawSelection 1 csel) ++ (drawSelection 5 sel)
            Nothing -> []
       ) ++
        [ circle
         [ r "20"
-        , onMouseDown (SetSelectionMode (Just { dist = 0, angle = 0}))
+        , onMouseDown (SelectionStart current.name)
+        , strokeWidth "2px"
         , stroke "black"
+        , fill (if (current.isOn) then (colorToCss color) else "black")
+        ] []
+       , circle
+        [ r "7"
+        , onMouseDown (SelectionStart current.name)
         , fill (colorToCss color)
         ] []
        ]
       )
+
+toPx : Float -> String
+toPx n = (toString n) ++ "px"
+
+drawSelection : Float -> Selection -> List (Svg Msg)
+drawSelection n sel =
+  [ circle
+      [ stroke (colorFromSelection sel |> toRgb |> colorToCss)
+      , strokeWidth (toPx n)
+      , fill "none"
+      , r (toString sel.dist)
+      ] []
+  , line
+      [ stroke (colorFromSelection sel |> toRgb |> colorToCss)
+      , strokeWidth (toPx n)
+      , x2 (toPx ((cos sel.angle) * sel.dist))
+      , y2 (toPx ((sin sel.angle) * sel.dist))
+      ] []
+  ]
 
 colorToCss : { d | blue : a, green : b, red : c } -> String
 colorToCss color =
