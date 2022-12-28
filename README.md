0ab80e8d856538446de81ecbc1caefb139dc28b8
Christian Gram Kalhauge <chrg@dtu.dk>
2022-12-28 08:47:55 +0100

    Some work done

--- README.md
+++ README.md
@@ -28,9 +28,32 @@ cargo run --release
 ## Log 
 
 - Followed the advice from [rp2040-project-template-nix](https://github.com/polygon/rp2040-project-template-nix/)
+- Install a CMSIS-DAP prope: You can use a second Pico as a CMSIS-DAP debug probe by installing the following firmware on it: https://github.com/majbthrd/DapperMime/releases/download/20210225/raspberry_pi_pico-DapperMime.uf2
 
 ## References
 
 More information about the [UF2](https://microsoft.github.io/uf2/)
+
+### WS2812
+How to get the WS2812 chip running with the pico:
+[here](https://core-electronics.com.au/guides/how-to-use-ws2812b-rgb-leds-with-raspberry-pi-pico/)
+Sadly, it's in micropython. But maybe we can improve
+
+```python
+@rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
+def ws2812():
+    T1 = 2
+    T2 = 5
+    T3 = 3
+    wrap_target()
+    label("bit_loop")
+    out(x, 1)               .side(0)    [T3 - 1]
+    jmp(not_x, "do_zero")   .side(1)    [T1 - 1]
+    jmp("bit_loop")          .side(1)    [T2 - 1]
+    label("do_zero")
+    nop()                   .side(0)    [T2 - 1]
+    wrap()
+```
+
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+use bsp::{entry, hal::pio::ShiftDirection};
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use embedded_time::fixed_point::FixedPoint;
+// use embedded_hal::digital::v2::OutputPin;
+// use embedded_time::fixed_point::FixedPoint;
 use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
@@ -17,8 +14,12 @@ use rp_pico as bsp;
 // use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
+    clocks::init_clocks_and_plls,
+    gpio::{FunctionPio0, Pin},
     pac,
+    pio::PIOBuilder,
+    pio::PIOExt,
+    pio::PinDir,
     sio::Sio,
     watchdog::Watchdog,
 };
@@ -27,13 +28,13 @@ use bsp::hal::{
 fn main() -> ! {
     info!("Program start");
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
+    // let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
+    let _clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
         pac.XOSC,
         pac.CLOCKS,
@@ -45,7 +46,7 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+    // let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
@@ -54,15 +55,44 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.gpio1.into_push_pull_output();
+    // setup
 
+    // configure LED pin for Pio0.
+    let _led: Pin<_, FunctionPio0> = pins.gpio3.into_mode();
+    // PIN id for use inside of PIO
+    let led_pin_id = 16;
+
+    // Define some simple PIO program.
+    let program = pio_proc::pio_asm!(
+        ".side_set 1",
+        ".wrap_target",
+        "bit_loop:",
+        "out x, 1         side 0 [2]", // T3 - 1
+        "jmp !x do_zero   side 1 [1]", // T1 - 1
+        "jmp bit_loop     side 1 [4]", // T2 - 1
+        "do_zero:",
+        "nop              side 0 [4]", // T2 - 1
+        ".wrap"
+    );
+
+    // Initialize and start PIO
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let installed = pio.install(&program.program).unwrap();
+    let (mut sm, _, mut tx) = PIOBuilder::from_program(installed)
+        .autopull(true)
+        .pull_threshold(24)
+        .out_shift_direction(ShiftDirection::Left)
+        .set_pins(led_pin_id, 1)
+        .buffers(bsp::hal::pio::Buffers::OnlyTx)
+        .clock_divisor_fixed_point(65535 / 1, 0)
+        .build(sm0);
+    sm.set_pindirs([(led_pin_id, PinDir::Output)]);
+    tx.write(0x00ff9933);
+    sm.start();
+
+    // PIO runs in background, independently from CPU
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(2000);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(2000);
+        cortex_m::asm::wfi();
     }
 }
 
